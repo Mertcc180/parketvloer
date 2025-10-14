9a74f918061fad9aa684ffc3f5164019615c255d
Othmaneaissaoui <othmaneaissaoui435@gmail.com>
2025-10-14 11:53:43 +0200

    plugins

---
+++ wordpress/wp-content/plugins/ninja-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'saturday-drive/ninja-forms',
+        'pretty_version' => 'dev-6d8815a725bfb203ed2c10b914032998b09e3ad9',
+        'version' => 'dev-6d8815a725bfb203ed2c10b914032998b09e3ad9',
+        'reference' => '6d8815a725bfb203ed2c10b914032998b09e3ad9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'saturday-drive/ninja-forms' => array(
+            'pretty_version' => 'dev-6d8815a725bfb203ed2c10b914032998b09e3ad9',
+            'version' => 'dev-6d8815a725bfb203ed2c10b914032998b09e3ad9',
+            'reference' => '6d8815a725bfb203ed2c10b914032998b09e3ad9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
